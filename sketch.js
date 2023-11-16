f6762b53f12f148f8e57d93b7b790ba070f3af0f
No-1-U-No <gabepj7125@outlook.com>
2023-11-16 11:29:34 -0600

    Added p5.js template

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// CS30 Major Project
+// Gabe PausJenssen
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX, mouseY, 100);
+}
