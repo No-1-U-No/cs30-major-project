f482919a4a9ff6655b88969605300f6385a6814b
No-1-U-No <gabepj7125@outlook.com>
2023-11-21 12:17:47 -0600

    Currently unsure how to start, but will figure out the starting point soon.

--- sketch.js
+++ sketch.js
@@ -5,11 +5,30 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// Changes to proposal.md must be discussed with Mr. Schellenberg
+
+class Layout {
+  constructor() {
+    this.questionX = width/2 - this.size;
+    this.questionY = height/2 - this.size;
+    this.size = map(50, 0, 100, 0, width);
+  }
+
+  question() {
+    square(this.x, this.y, this.size);
+  }
+}
+
+let songs = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 100);
+}
+
+function question1() {
+  
 }
