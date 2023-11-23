a33f84ef0b083a384093be3b78120b25860ca24a
No-1-U-No <gabepj7125@outlook.com>
2023-11-23 11:14:42 -0600

    Deciding to let user choose between light or dark mode, but needs a bit of modification.

--- sketch.js
+++ sketch.js
@@ -7,37 +7,41 @@
 
 // Changes to proposal.md must be discussed with Mr. Schellenberg
 
-let question = "Start";
-
+let screenBrightness;
+let questionScreen = "Start";
+let colours = ["red", "blue", "green", "purple"];
 let songs = [];
 
-let question1;
+let firstQuestion;
 
 class Layout {
   constructor() {
+    this.space = map(2.5, 0, 100, 0, width);
+
     this.questionX = width/4;
     this.questionY = map(5, 0, 100, 0, height);
     this.questionWidth = width/2;
     this.questionHeight = height/4;
 
-    this.gap = map(2.5, 0, 100, 0, width);
-
-    this.choicesWidth = width/4 - map(2.5, 0, 100, 0, width);
-    this.choicesHeight = height/8;
-
     this.choices1and3x = width/4;
     this.choices2and4x = width/2 + map(2.5, 0, 100, 0, width);
 
     this.choices1and2y = height/4 + 2*map(2.5, 0, 100, 0, width);
-    this.choices3and4y = height/4 + height/8 + 3*map(2.5, 0, 100, 0, width);
+    this.choices3and4y = 3*height/8 + 3*map(2.5, 0, 100, 0, width);
 
     this.choicesWidth = width/4 - map(2.5, 0, 100, 0, width);
     this.choicesHeight = height/8;
+
+    this.backX = width/4;
+    this.nextX = width/2 + 2*map(2.5, 0, 100, 0, width);
+
+    this.backNextY = map(90, 0, 100, 0, height);
+    this.backNextWidth = width/5;
+    this.backNextHeight = height/16;
   }
 
   question() {
     noStroke();
-    fill("red");
     rect(this.questionX, this.questionY, this.questionWidth, this.questionHeight);
   }
 
@@ -56,6 +60,15 @@ class Layout {
   choice4() {
     rect(this.choices2and4x, this.choices3and4y, this.choicesWidth, this.choicesHeight);
   }
+
+  back() {
+    fill("black");
+    rect(this.backX, this.backNextY, this.backNextWidth, this.backNextHeight);
+  }
+
+  next() {
+    rect(this.nextX, this.backNextY, this.backNextWidth, this.backNextHeight);
+  }
 }
 
 function setup() {
@@ -67,11 +80,33 @@ function windowResized() {
 }
 
 function draw() {
-  background("whitesmoke");
-  question1 = new Layout;
-  question1.question();
-  question1.choice1();
-  question1.choice2();
-  question1.choice3();
-  question1.choice4();
+  preference();
+}
+
+function preference() {
+  let screenBrightness = prompt("Do you want to take this quiz in light mode or dark mode?");
+
+  if (screenBrightness === "l") {
+    background("whitesmoke");
+  }
+
+  else {
+    background(0);
+  }
+}
+
+function question1() {
+  let firstQuestion = new Layout;
+  fill(colours[0]);
+  firstQuestion.question();
+  firstQuestion.choice1();
+  firstQuestion.choice2();
+  firstQuestion.choice3();
+  firstQuestion.choice4();
+  firstQuestion.back();
+  firstQuestion.next();
+}
+
+function questions() {
+  question1();
 }
